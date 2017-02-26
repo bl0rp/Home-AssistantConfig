e2995f3cb72d86beda97a8f4cc0eb0cafd0540d4
bl0rp <c.github@0x711.de>
2017-02-26 15:48:38 +0100

    minor update

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-cd /home/homeassistant/.homeassistant
-source /srv/homeassistant/homeassistant_venv/bin/activate
-hass --script check_config
-
 git add .
 git status
 echo -n "Enter the Description for the Change: " [Minor Update]
