7e05ad0e7a7a2130d2cb72b7d5a175bf30b2c7da
bl0rp <c.github@0x711.de>
2021-08-11 06:35:38 +0200

    clean commit before zwave migration

--- custom_components/hacs/__init__.py
+++ custom_components/hacs/__init__.py
@@ -6,25 +6,27 @@ https://hacs.xyz/
 """
 import voluptuous as vol
 
-from custom_components.hacs.const import DOMAIN
-from custom_components.hacs.helpers.functions.configuration_schema import (
-    hacs_config_combined,
-)
+from .const import DOMAIN
+from .helpers.functions.configuration_schema import hacs_config_combined
+from .operational.setup import async_setup as hacs_yaml_setup
+from .operational.setup import async_setup_entry as hacs_ui_setup
+from .operational.remove import async_remove_entry as hacs_remove_entry
 
 CONFIG_SCHEMA = vol.Schema({DOMAIN: hacs_config_combined()}, extra=vol.ALLOW_EXTRA)
 
 
 async def async_setup(hass, config):
     """Set up this integration using yaml."""
-    from custom_components.hacs.operational.setup import async_setup as hacs_yaml_setup
 
     return await hacs_yaml_setup(hass, config)
 
 
 async def async_setup_entry(hass, config_entry):
     """Set up this integration using UI."""
-    from custom_components.hacs.operational.setup import (
-        async_setup_entry as hacs_ui_setup,
-    )
 
     return await hacs_ui_setup(hass, config_entry)
+
+
+async def async_remove_entry(hass, config_entry):
+    """Handle removal of an entry."""
+    return await hacs_remove_entry(hass, config_entry)
